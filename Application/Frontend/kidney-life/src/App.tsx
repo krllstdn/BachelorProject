9a54a21087b3a11894bebaf421ccdde4b72dc412
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-14 10:31:50 +0200

    Remove redundancies

--- Application/Frontend/kidney-life/src/App.tsx
+++ Application/Frontend/kidney-life/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/sidebar';
 import NavBar from './components/navbar';
 import AddNewPair from './components/addNewPair';
 import AddNewPairNew from './components/addNewPair';
+import { AddNewPairExisting } from './components/addNewPair';
 
 function App() {
   return (
-      <body className="App-body">
+      <div className="App-body">
         <div className="navbar">
           <NavBar />
         </div>
@@ -16,9 +16,10 @@ function App() {
           <SideBar />
         </div>
         <div className="add-new-pair">
+          {/* <AddNewPairExisting /> */}
           <AddNewPairNew />
         </div>
-      </body>
+      </div>
   );
 }
 
