514b6b09505bf6a9d99a952e7b66575956054b68
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-21 11:39:20 +0200

    DRY: move constant from DashboardPage

--- Application/Frontend/kidney-life/src/components/pages/DashboardPage.tsx
+++ Application/Frontend/kidney-life/src/components/pages/DashboardPage.tsx
@@ -6,7 +6,7 @@ import AddNewPairExisting from "../modals/AddNewPairExisting";
 import AddNewPair from "../modals/AddNewPair";
 import InfoDisplay from "../cards/InfoDisplay";
 import ParamDisplay from "../cards/ParamDisplay";
-import { Tab } from "../forms/Tabs";
+import { tabPair, infoPrediction } from "../../helpers/constants";
 import {
   formTypes,
   infoDataDonor,
@@ -17,16 +17,6 @@ import ConfirmDelete from "../modals/ConfirmDelete";
 import EditPatient from "../forms/EditPatient";
 
 function DashboardPage() {
-  const infoPrediction = {
-    header: "Prediction",
-    fields: {
-      Probability: "0.8",
-      Confidence: "0.9",
-      Years: "5.3",
-      "Risk Score": "0.7",
-    },
-  };
-
   const VIEWS = {
     NONE: "NONE",
     ADD_NEW_PAIR: "ADD_NEW_PAIR",
@@ -66,34 +56,6 @@ function DashboardPage() {
   const handleOpenEditRecipient = () => {
     setCurrentView(VIEWS.EDIT_RECIPIENT);
   };
-
-  const sidebarItemsData = [
-    {
-      "Pair id": 123,
-      "Donor Blood Group": "A",
-      "Recipient Blood Group": "B",
-      Type: "Deceased",
-    },
-    {
-      "Pair id": 124,
-      "Donor Blood Group": "A",
-      "Recipient Blood Group": "B",
-      Type: "Living",
-    },
-    {
-      "Pair id": 125,
-      "Donor Blood Group": "A",
-      "Recipient Blood Group": "B",
-      Type: "Living",
-    },
-  ];
-
-  const tab: Tab[] = [
-    {
-      title: "Pairs",
-      content: sidebarItemsData,
-    },
-  ];
 
   return (
     <div className="App-body">
@@ -136,7 +98,7 @@ function DashboardPage() {
       </div>
       <div className="main flex pr-5 w-screen">
         <div className="sidebar inline-block">
-          <SideBar tabs={tab} onButtonClick={handleOpenAddNewPair} />
+          <SideBar tabs={tabPair} onButtonClick={handleOpenAddNewPair} />
         </div>
         <div className="w-full">
           <div className="flex justify-between">
