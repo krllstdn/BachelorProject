eb5bfe8f31b073d750f56e3d7bbc0639f348a7d8
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-21 15:41:34 +0200

    Add graph to the dashboard

--- Application/Frontend/kidney-life/src/components/layout/Navbar.tsx
+++ Application/Frontend/kidney-life/src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-    <div className="flex justify-between items-center w-screen h-20 bg-primary text-white mb-3">
+    <div className="flex justify-between items-center w-screen h-20 bg-primary text-white mb-6">
       <div className="font-bold text-2xl ml-10">KidneyLife</div>
       <div className="space-x-10 mr-10 text-lg">
         <a href="#">Dashboard</a>
