a57ac167856cec6a8641685a587a3dba486dfc96
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-21 16:22:56 +0200

    Add RegisterPage draft

--- Application/Frontend/kidney-life/src/App.tsx
+++ Application/Frontend/kidney-life/src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import DashboardPage from "./components/pages/DashboardPage";
 import PatientsPage from "./components/pages/PatientsPage";
 import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 function App() {
   return (
     <div className="App">
-      <DashboardPage />
+      {/* <DashboardPage /> */}
       {/* <PatientsPage /> */}
       {/* <LoginPage /> */}
+      <RegisterPage />
     </div>
   );
 }
