9a419f9d444e447da9968e101624e9e89ef901b6
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-21 11:38:08 +0200

    Fix buttons paddings

--- Application/Frontend/kidney-life/src/components/modals/ConfirmDelete.tsx
+++ Application/Frontend/kidney-life/src/components/modals/ConfirmDelete.tsx
@@ -12,17 +12,21 @@ type ConfirmDeleteProps = {
 function ConfirmDelete(props: ConfirmDeleteProps) {
   return (
     <BlurredBackdrop>
-      <ModalContainer className="w-1/2 pr-5 pl-5 pt-5 pb-8">
+      <ModalContainer className="w-1/2 pr-5 pl-5 pt-5 pb-8 xl:w-1/3">
         <h1 className="text-center text-2xl font-semibold mb-3">
           Are you sure you want to delete this?
         </h1>
         <div className="flex justify-evenly">
           <Button
             name="Yes"
-            additionalStyles="w-24"
+            additionalStyles="w-28 p-2"
             onClick={props.onClickYes}
           />
-          <Button name="No" additionalStyles="w-24" onClick={props.onClickNo} />
+          <Button
+            name="No"
+            additionalStyles="w-28 p-2"
+            onClick={props.onClickNo}
+          />
         </div>
         <CloseButton onClick={props.onClose} styles="pr-4 pt-2" />
       </ModalContainer>
