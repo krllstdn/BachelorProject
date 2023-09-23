2a5ccb9a9b72b1d9028995bcee90726e98129a9a
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-23 13:05:42 +0200

    Handle CORS

--- Application/Backend/KidneyLife/settings.py
+++ Application/Backend/KidneyLife/settings.py
@@ -47,12 +47,12 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 INTERNAL_IPS = [
