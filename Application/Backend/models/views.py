ba64731a6a2b18605e600d860828d0d8c764b78e
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-24 10:37:09 +0200

    Create models app

---
+++ Application/Backend/models/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Model
+from .serializers import ModelSerializer
+
+
+class ModelList(generics.ListCreateAPIView):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
