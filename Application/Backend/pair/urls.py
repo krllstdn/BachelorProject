ebe7d82cc8c6ddd64a58ef22a58530f9a8df7a3e
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-25 09:14:08 +0200

    CRUD for pair

--- Application/Backend/pair/urls.py
+++ Application/Backend/pair/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import PairList
+from .views import PairList, PairRetrieveUpdateDestroy
 
 urlpatterns = [
     path("", PairList.as_view()),
+    path(
+        "<int:pk>/",
+        PairRetrieveUpdateDestroy.as_view(),
+        name="pair-retrieve-update-destroy",
+    ),
 ]
