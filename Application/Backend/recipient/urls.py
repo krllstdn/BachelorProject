91b159fe69e7a4d6a454a503fed0ebf70c5db179
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-25 09:15:02 +0200

    CRUD for Recipient

--- Application/Backend/recipient/urls.py
+++ Application/Backend/recipient/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RecipientList
+from .views import RecipientList, RecipientRetrieveUpdateDestroy
 
 urlpatterns = [
     path("", RecipientList.as_view()),
+    path("<int:pk>/", RecipientRetrieveUpdateDestroy.as_view()),
 ]
