c96292b5a1bf837d38ee7832b0266f8f99c5fada
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-25 09:07:43 +0200

    CRUD for model

--- Application/Backend/models/views.py
+++ Application/Backend/models/views.py
@@ -8,3 +8,8 @@ from .serializers import ModelSerializer
 class ModelList(generics.ListCreateAPIView):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
+
+
+class ModelRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
