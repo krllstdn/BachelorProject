1a38a16e20cd7760ed7fbd2ea65900b9e1f4f22e
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-25 09:07:43 +0200

    CRUD for model

--- Application/Backend/models/urls.py
+++ Application/Backend/models/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ModelList
+from .views import ModelList, ModelRetrieveUpdateDestroy
 
 urlpatterns = [
     path("", ModelList.as_view()),
+    path(
+        "<int:pk>/",
+        ModelRetrieveUpdateDestroy.as_view(),
+        name="model-retrieve-update-destroy",
+    ),
 ]
