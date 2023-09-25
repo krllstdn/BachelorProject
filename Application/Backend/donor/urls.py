d28e4883a42c60f54bc36fab95e11830d1758d60
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-25 09:01:45 +0200

    CRUD for donor

--- Application/Backend/donor/urls.py
+++ Application/Backend/donor/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import DonorList
+from .views import DonorList, CreateDonor, DonorRetrieveUpdateDestroy
 
 urlpatterns = [
     path("", DonorList.as_view()),
+    path("create/", CreateDonor.as_view()),
+    path(
+        "<int:pk>/",
+        DonorRetrieveUpdateDestroy.as_view(),
+        name="donor-retrieve-update-destroy",
+    ),
 ]
