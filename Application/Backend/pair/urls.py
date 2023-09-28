a2616162fdad5035fc96d784236e420e47e8e26f
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-28 09:34:16 +0200

    Add detailed pair endpoint

--- Application/Backend/pair/urls.py
+++ Application/Backend/pair/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PairList, PairRetrieveUpdateDestroy
+from .views import PairList, PairRetrieveUpdateDestroy, GetDetailedPair
 
 urlpatterns = [
     path("", PairList.as_view()),
@@ -8,4 +8,5 @@ urlpatterns = [
         PairRetrieveUpdateDestroy.as_view(),
         name="pair-retrieve-update-destroy",
     ),
+    path("detail/<int:pk>/", GetDetailedPair.as_view(), name="pair-detail"),
 ]
