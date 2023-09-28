3be8a17450071fab5fb39bc71d182be46de62c5c
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-28 09:34:16 +0200

    Add detailed pair endpoint

--- Application/Backend/pair/models.py
+++ Application/Backend/pair/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Pair(models.Model):
     pair_id = models.IntegerField()
-    recipient_id = models.ForeignKey("recipient.Recipient", on_delete=models.CASCADE)
-    donor_id = models.ForeignKey("donor.Donor", on_delete=models.CASCADE)
+    recipient = models.ForeignKey("recipient.Recipient", on_delete=models.CASCADE)
+    donor = models.ForeignKey("donor.Donor", on_delete=models.CASCADE)
