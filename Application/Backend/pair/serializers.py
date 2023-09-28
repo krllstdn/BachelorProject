1fbcae332c0d0451d6a70ca5c5c6da29a978f5c3
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-28 09:34:16 +0200

    Add detailed pair endpoint

--- Application/Backend/pair/serializers.py
+++ Application/Backend/pair/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import Pair
+from donor.serializers import DonorSerializer
+from recipient.serializers import RecipientSerializer
 
 
 class PairSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pair
-        fields = ("pair_id", "donor_id", "recipient_id")
+        fields = ("pair_id", "donor", "recipient")
+
+
+class PairDetailSerializer(serializers.ModelSerializer):
+    donor = DonorSerializer()
+    recipient = RecipientSerializer()
+
+    class Meta:
+        model = Pair
+        fields = ("pair_id", "donor", "recipient")
--- Application/Backend/pair/views.py
+++ Application/Backend/pair/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Pair
-from .serializers import PairSerializer
+from .serializers import PairSerializer, PairDetailSerializer
 
 
 class PairList(generics.ListCreateAPIView):
@@ -13,3 +13,8 @@ class PairList(generics.ListCreateAPIView):
 class PairRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pair.objects.all()
     serializer_class = PairSerializer
+
+
+class GetDetailedPair(generics.RetrieveAPIView):
+    queryset = Pair.objects.all()
+    serializer_class = PairDetailSerializer
