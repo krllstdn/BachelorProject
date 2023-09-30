b77d87312b65b23977dfef7cb135b26caa7f53bc
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-09-30 11:39:12 +0200

    Add ids for recipients, donors and pairs

--- Application/Backend/pair/models.py
+++ Application/Backend/pair/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Pair(models.Model):
-    pair_id = models.IntegerField()
+    pair_id = models.AutoField(primary_key=True)
     recipient = models.ForeignKey("recipient.Recipient", on_delete=models.CASCADE)
     donor = models.ForeignKey("donor.Donor", on_delete=models.CASCADE)
