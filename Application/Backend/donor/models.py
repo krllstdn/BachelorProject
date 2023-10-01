25dba9ee827624b06ff62df6f455cc2851729e39
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-10-01 11:35:41 +0200

    Make donor and recipient ids autoincrement

--- Application/Backend/donor/models.py
+++ Application/Backend/donor/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-import uuid
 
 
 class Donor(models.Model):
-    donor_id = models.IntegerField(null=False, unique=True)
+    donor_id = models.AutoField(primary_key=True)
     donor_data = models.JSONField()
