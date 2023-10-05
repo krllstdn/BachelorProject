8d3b096fcdbf5826d366858eb86ac9d672186b3b
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-10-05 09:31:09 +0200

    Fix ConfirmDelete

--- Application/Frontend/kidney-life/src/components/modals/ConfirmDelete.tsx
+++ Application/Frontend/kidney-life/src/components/modals/ConfirmDelete.tsx
@@ -12,19 +12,20 @@ type ConfirmDeleteProps = {
 function ConfirmDelete(props: ConfirmDeleteProps) {
   return (
     <BlurredBackdrop>
-      <ModalContainer className="w-1/2 pr-5 pl-5 pt-5 pb-8 xl:w-1/3">
+      <ModalContainer className="w-1/2 p-8 xl:w-1/3">
         <h1 className="text-center text-2xl font-semibold mb-3">
-          Are you sure you want to delete this?
+          Are you sure you want to delete this patient? All pairs associated
+          with this patient will also be deleted.
         </h1>
         <div className="flex justify-evenly">
           <Button
             name="Yes"
-            additionalStyles="w-28 p-2"
+            additionalStyles="pr-9 pl-9 p-3"
             onClick={props.onClickYes}
           />
           <Button
             name="No"
-            additionalStyles="w-28 p-2"
+            additionalStyles="pr-9 pl-9 p-3"
             onClick={props.onClickNo}
           />
         </div>
