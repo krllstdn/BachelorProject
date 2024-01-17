7021d3d7e27c9b67b19156426b48f4b2e90e13a1
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-01-17 19:58:43 +0100

    Cleaned FE

--- Application/Frontend/kidney-life/src/App.tsx
+++ Application/Frontend/kidney-life/src/App.tsx
@@ -1,38 +1,12 @@
 import "./App.css";
-import DashboardPage from "./components/pages/DashboardPage";
-import PatientsPage from "./components/pages/PatientsPage";
-// import LoginPage from "./components/pages/LoginPage";
-import RegisterPage from "./components/pages/RegisterPage";
 import PredictPage from "./components/pages/PredictPage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Outlet,
-} from "react-router-dom";
-import { PatientProvider } from "./context/patientsPageContext";
-
-function PatientContextLayout() {
-  return (
-    <PatientProvider>
-      <Outlet />
-    </PatientProvider>
-  );
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route element={<PatientContextLayout />}>
-            <Route path="/patients" Component={PatientsPage} />
-            <Route path="/" Component={DashboardPage} />
-          </Route>
-          {/* <Route path="/login" Component={LoginPage} /> */}
-          <Route path="/register" Component={RegisterPage} />
-
           <Route path="/predict" Component={PredictPage} />
         </Routes>
       </Router>
