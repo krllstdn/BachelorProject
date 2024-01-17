66fa3bfa4eaff4472b462b9dc713b00f25b94bc6
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-01-17 19:00:12 +0100

    Make code cleaner

---
+++ Application/Backend/prediction/utils.py
@@ -0,0 +1,118 @@
+import pickle, os, json
+
+import numpy as np
+
+from django.conf import settings
+
+
+MODELS = {
+    "COXNET_DECEASED": "coxnet_deceased_desc.json",
+    "COXNET_LIVING": "coxnet_living_desc.json",
+}
+
+
+def load_model_description(model):
+    """
+    Load model description for the provided model name.
+
+    Args:
+        model (str): Model name.
+
+    Returns:
+        tuple: Features, model pickle path, pipeline pickle path.
+    """
+    file_path = os.path.join(settings.BASE_DIR, "models", "models", MODELS[model])
+    try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            return (
+                data["features"],
+                _get_model_path(data["model_file_name"]),
+                data["pipeline_file_name"],
+            )
+    except FileNotFoundError:
+        raise ValueError(f"Model description file {model} not found")
+
+
+def _get_model_path(model_file_name):
+    return os.path.join(settings.BASE_DIR, "models", "models", model_file_name)
+
+
+def load_model(model_path):
+    """
+    Load model pickle from the provided path.
+
+    Args:
+        model_path (str): Path to the model pickle.
+
+    Returns:
+        model: Loaded model.
+    """
+    try:
+        with open(model_path, "rb") as model_file:
+            model = pickle.load(model_file)
+        return model
+    except FileNotFoundError:
+        return Response(
+            {"error": "Model pickle not found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+
+def generate_synthetic_data(features):
+    """
+    Generate synthetic data based on the provided features.
+
+    Args:
+        features (list): List of feature dictionaries.
+
+    Returns:
+        dict: Synthetic data.
+    """
+    synthetic_data = {}
+    for feature in features:
+        if feature["type"] == "categorical":
+            synthetic_data[feature["name"]] = generate_synthetic_categorical_data(
+                feature
+            )
+        elif feature["type"] == "numerical":
+            synthetic_data[feature["name"]] = generate_synthetic_numerical_data(feature)
+    return synthetic_data
+
+
+def generate_synthetic_categorical_data(feature):
+    """
+    Generate synthetic categorical data based on the provided feature description.
+
+    Args:
+        feature (dict): Feature description.
+
+    Returns:
+        Str: Synthetic categorical data.
+    """
+    freq_dict = feature["freq"]
+    categories = list(freq_dict.keys())
+    probabilities = list(freq_dict.values())
+
+    synthetic_data = np.random.choice(categories, size=1, p=probabilities)[0]
+
+    return synthetic_data
+
+
+def generate_synthetic_numerical_data(feature):
+    """
+    Generate synthetic numerical data based on the provided feature description.
+
+    Args:
+        feature (dict): Feature description.
+
+    Returns:
+        float: Synthetic numerical data.
+    """
+    synthetic_data = np.random.normal(
+        loc=feature["stats"]["median"], scale=feature["stats"]["IQR"], size=1
+    )[0]
+    synthetic_data = np.clip(synthetic_data, 0, None)
+    synthetic_data = np.round(synthetic_data)
+
+    return synthetic_data
--- Application/Backend/prediction/views.py
+++ Application/Backend/prediction/views.py
@@ -11,6 +11,7 @@ import numpy as np
 
 from .models import Prediction
 from .serializers import PredictionSerializer, CoxnetDeceasedSerializer
+from .utils import load_model_description, load_model, MODELS, generate_synthetic_data
 
 
 class PredictionList(generics.ListCreateAPIView):
@@ -18,33 +19,19 @@ class PredictionList(generics.ListCreateAPIView):
     serializer_class = PredictionSerializer
 
 
-MODELS = {
-    "COXNET_DECEASED": "coxnet_deceased_desc.json",
-    "COXNET_LIVING": "coxnet_living_desc.json",
-}
-
-
-def load_features_json(model):
-    file_path = os.path.join(settings.BASE_DIR, "models", "models", MODELS[model])
-    try:
-        with open(file_path, "r") as f:
-            data = json.load(f)
-            return (
-                data["features"],
-                data["model_file_name"],
-                data["pipeline_file_name"],
-            )
-    except FileNotFoundError:
-        raise ValueError(f"Model description file {model} not found")
-
-
 class PredictAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        model_name = request.data["model_name"]
+        try:
+            model_name = request.data["model_name"]
+            data = request.data["features"]
+        except KeyError:
+            return Response(
+                {"error": "Missing 'model_name' or 'features' in request data"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        features, model_file_name, pipeline_file_name = load_features_json(model_name)
-        model_pickle_path = os.path.join(
-            settings.BASE_DIR, "models", "models", model_file_name
+        features, model_pickle_path, pipeline_file_name = load_model_description(
+            model_name
         )
 
         serializer = CoxnetDeceasedSerializer(
@@ -54,15 +41,7 @@ class PredictAPIView(APIView):
         )
 
         if serializer.is_valid():
-            try:
-                with open(model_pickle_path, "rb") as model_file:
-                    model = pickle.load(model_file)
-            except FileNotFoundError:
-                return Response(
-                    {"error": "Model pickle not found"},
-                    status=status.HTTP_404_NOT_FOUND,
-                )
-
+            model = load_model(model_pickle_path)
             data = serializer.validated_data
 
             prediction = model.predict_survival_function(data)
@@ -81,39 +60,16 @@ class PredictAPIView(APIView):
 
 class RandomDataAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        model_name = request.data["model_name"]
+        try:
+            model_name = request.data["model_name"]
+        except KeyError:
+            return Response(
+                {"error": "Missing 'model_name' in request data"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        features, _, _ = load_features_json(model_name)
+        features, _, _ = load_model_description(model_name)
 
-        synthetic_data = {}
-        for feature in features:
-            if feature["type"] == "categorical":
-                synthetic_data[feature["name"]] = generate_synthetic_categorical_data(
-                    feature
-                )
-            elif feature["type"] == "numerical":
-                synthetic_data[feature["name"]] = generate_synthetic_numerical_data(
-                    feature
-                )
+        synthetic_data = generate_synthetic_data(features)
 
         return Response(synthetic_data)
-
-
-def generate_synthetic_categorical_data(feature):
-    freq_dict = feature["freq"]
-    categories = list(freq_dict.keys())
-    probabilities = list(freq_dict.values())
-
-    synthetic_data = np.random.choice(categories, size=1, p=probabilities)[0]
-
-    return synthetic_data
-
-
-def generate_synthetic_numerical_data(feature):
-    synthetic_data = np.random.normal(
-        loc=feature["stats"]["median"], scale=feature["stats"]["IQR"], size=1
-    )[0]
-    synthetic_data = np.clip(synthetic_data, 0, None)
-    synthetic_data = np.round(synthetic_data)
-
-    return synthetic_data
