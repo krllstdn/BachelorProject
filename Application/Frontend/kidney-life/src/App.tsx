08de9a4cddc7c85a29430b9729ce4b04dfbf582c
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-01-27 22:49:33 +0100

    Minor refinements

--- Application/Frontend/kidney-life/src/App.tsx
+++ Application/Frontend/kidney-life/src/App.tsx
@@ -1,15 +1,9 @@
 import "./App.css";
 import PredictPage from "./components/pages/PredictPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Routes>
-          <Route path="/predict" Component={PredictPage} />
-        </Routes>
-      </Router> */}
       <PredictPage />
     </div>
   );
