3701733dd57ebcda6c490c950efa9180ff3ae48d
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-01-27 22:59:13 +0100

    Remove redundant comments from settings.py

--- Application/Backend/KidneyLife/settings.py
+++ Application/Backend/KidneyLife/settings.py
@@ -45,15 +45,6 @@ HOST = os.environ.get("SERVER_IP")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-#vska1kj6vi&4^wf9!1vfxw0*=u)65f-v65(18d=ot6)woks=c"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", HOST]
 
 
