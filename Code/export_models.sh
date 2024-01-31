823ef6c8226fffe57dcc6d44d74fec289dd121bf
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-01-31 11:47:40 +0100

    Retrain coxnet_living model

--- Code/export_models.sh
+++ Code/export_models.sh
@@ -1,9 +1,10 @@
 
 echo "Copying coxnet living model and pipeline..."
 cp pickle/COXNET_LIVING_MODEL.pickle ../Application/models/COXNET_LIVING_MODEL.pickle
-cp pickle/pipeline_coxnet_living.pkl ../Application/Backend/prediction/pickles/pipeline_coxnet_living.pkl
+# cp pickle/COXNET_LIVING_MODEL.pickle ../Application/Backend/prediction/models/COXNET_LIVING_MODEL.pickle
+cp pickle/pipeline_coxnet_living.pkl ../Application/Backend/prediction/pickles/coxnet_living_pipeline.pkl
 
 echo "Copying coxnet deceased model and pipeline..."
 cp pickle/COXNET_DECEASED_MODEL.pickle ../Application/models/COXNET_DECEASED_MODEL.pickle
-cp pickle/trained_pipeline.pkl ../Application/Backend/prediction/pickles/pipeline_coxnet_deceased.pkl
-
+# cp pickle/COXNET_DECEASED_MODEL.pickle ../Application/Backend/prediction/models/COXNET_DECEASED_MODEL.pickle
+cp pickle/trained_pipeline.pkl ../Application/Backend/prediction/pickles/pipeline_coxnet_deceased_pipeline.pkl
