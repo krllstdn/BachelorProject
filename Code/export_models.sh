b7e14d21ef73c5a98e505b254d43b921d88ff4ed
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-02-01 14:32:42 +0100

    Fix wrong descriptions

--- Code/export_models.sh
+++ Code/export_models.sh
@@ -1,10 +1,14 @@
 
 echo "Copying coxnet living model and pipeline..."
 cp pickle/COXNET_LIVING_MODEL.pickle ../Application/models/COXNET_LIVING_MODEL.pickle
-# cp pickle/COXNET_LIVING_MODEL.pickle ../Application/Backend/prediction/models/COXNET_LIVING_MODEL.pickle
+cp pickle/COXNET_LIVING_MODEL.pickle ../Application/Backend/prediction/models/COXNET_LIVING_MODEL.pickle
 cp pickle/pipeline_coxnet_living.pkl ../Application/Backend/prediction/pickles/coxnet_living_pipeline.pkl
 
 echo "Copying coxnet deceased model and pipeline..."
 cp pickle/COXNET_DECEASED_MODEL.pickle ../Application/models/COXNET_DECEASED_MODEL.pickle
-# cp pickle/COXNET_DECEASED_MODEL.pickle ../Application/Backend/prediction/models/COXNET_DECEASED_MODEL.pickle
-cp pickle/trained_pipeline.pkl ../Application/Backend/prediction/pickles/pipeline_coxnet_deceased_pipeline.pkl
+cp pickle/COXNET_DECEASED_MODEL.pickle ../Application/Backend/prediction/models/COXNET_DECEASED_MODEL.pickle
+cp pickle/trained_pipeline.pkl ../Application/Backend/prediction/pickles/coxnet_deceased_pipeline.pkl
+
+echo "Copying model descriptions..."
+cp ../Application/models/coxnet_living_desc.json ../Application/Backend/prediction/models/coxnet_living_desc.json
+cp ../Application/models/coxnet_deceased_desc.json ../Application/Backend/prediction/models/coxnet_deceased_desc.json
