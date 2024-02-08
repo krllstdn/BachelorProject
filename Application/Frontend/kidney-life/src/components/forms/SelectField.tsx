b129c82e537f23a4bca561aa3f0a29df968c7edd
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-02-08 13:07:56 +0100

    Make FE responsive up until xl and notebook screen

--- Application/Frontend/kidney-life/src/components/forms/SelectField.tsx
+++ Application/Frontend/kidney-life/src/components/forms/SelectField.tsx
@@ -49,10 +49,10 @@ type SelectFieldCoxProps = {
 
 export function SelectFieldCox(props: SelectFieldCoxProps) {
   return (
-    <div className="bg-quaternary rounded p-3 text-left flex flex-start mb-1 mt-3 text-sm">
+    <div className="bg-quaternary rounded p-3 md:pl-2 md:pr-2 text-left flex flex-start mb-1 mt-3 text-sm">
       <label
         htmlFor={props.name}
-        className="text-primary  font-semibold mr-3 whitespace-nowrap"
+        className="text-primary  font-medium mr-3 whitespace-nowrap"
       >
         {props.text + ":"}
       </label>
