dcff24dfbdf473198c7ba3350db75ce207b55eff
Kyrylo Stadniuk <krllstdn@gmail.com>
2024-08-05 23:26:30 +0200

    Organize the repo

--- Code/main.py
+++
@@ -1,9 +0,0 @@
-from regressor_models import Regressors
-from process_data import DataProcessor
-
-
-if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = DataProcessor()
-
-    Regressors(X_train, X_test, y_train, y_test)
-
